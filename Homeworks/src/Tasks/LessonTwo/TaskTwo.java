6e40d9bcf4f07624dc61bb0ae05a9279bb5d1df8
pkabanov <pkabanov@axiomsl.com>
2020-05-30 10:20:59 +0300

    Lesson 2 Task 2 updated Added logic for equal input and for input equidistant from 10

--- Homeworks/src/Tasks/LessonTwo/TaskTwo.java
+++ Homeworks/src/Tasks/LessonTwo/TaskTwo.java
@@ -4,8 +4,14 @@ public class TaskTwo {
     //    Создать программу, выводящую на экран ближайшее к 10 из двух положительных чисел, записанных в переменные m и n. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
     public static void nearTen(double a, double b) {
         double constant = 10;
+        if (a == b){
+            System.out.println("Вы ввели два одинаковых числа, соответственно они равноудалены от 10.");
+        } else if (Math.abs(10 - a) == Math.abs(10 - b)){
+            System.out.println("Введенные числа равноудалены от 10.");
+        } else {
 //        Решил сделать через условную операцию, но на сколько я понимаю под капотом там тот же if
-        double result = Math.abs(constant - a) < Math.abs(constant - b) ? a : b;
-        System.out.println("К 10 ближе число: " + result);
+            double result = Math.abs(constant - a) < Math.abs(constant - b) ? a : b;
+            System.out.println("К 10 ближе число: " + result);
+        }
     }
 }
